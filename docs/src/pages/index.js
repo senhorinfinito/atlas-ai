158a6b82860d7f3ca5413d6a12ebc58564b6ad25
senhorinfinito <anantsakhare11@gmail.com>
2025-07-31 23:18:09 +0530

    added basic docs details

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/Overview"  style={{ backgroundColor: "#c4e6c5ff" }}>
+            to="/docs/Overview"  style={{ backgroundColor: "#a6b1a6ff" }}>
             Get Started with Atlas
           </Link>
         </div>
